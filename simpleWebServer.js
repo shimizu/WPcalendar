ad3a3d63e2e54d191a5e2f09b5fa602fb14f3004
shimizu <xxshimizuxx@gmail.com>
2013-07-30 11:59:33 +0900

    Update simpleWebServer.js

--- simpleWebServer.js
+++ simpleWebServer.js
@@ -1,7 +1,7 @@
 var http = require("http"),
     url = require("url"),
     path = require("path"),
-    fs = require("fs")
+    fs = require("fs"),
     port = process.argv[2] || 8888;
  
      
@@ -10,7 +10,7 @@ http.createServer(function(request, response) {
   var uri = url.parse(request.url).pathname
     , filename = path.join(process.cwd(), uri);
    
-    path.exists(filename, function(exists){
+    fs.exists(filename, function(exists){
         if (!exists) { Response_404(); return ; }
         if (fs.statSync(filename).isDirectory()) { filename += '/index.html'; }
  
